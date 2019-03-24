c5caca02ac4d1c70e1441535363fbb60cb571810
Monika <monika.stobienia@gmail.com>
2019-03-24 23:03:29 +0100

    first commit

---
+++ FoodPin/FoodPin/GalleryViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GalleryViewController.swift
+//  FoodPin
+//
+//  Created by stud on 28/01/2019.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class GalleryViewController: UIViewController {
+
+    @IBOutlet weak var booksButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
